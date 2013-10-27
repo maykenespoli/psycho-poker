ae1e819952b68e6057de5922bfda9f9fa8049947
maykenespoli <xmayke@yahoo.com.br>
2013-10-27 19:56:41 -0200

    Created Pair and Three of a kind matcher

---
+++ src/main/java/br/com/amil/poker/domain/hand/game/ThreeOfAKindMatcher.java
@@ -0,0 +1,30 @@
+package br.com.amil.poker.domain.hand.game;
+
+import br.com.amil.poker.domain.deck.Card;
+import br.com.amil.poker.domain.hand.OrderedHand;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class ThreeOfAKindMatcher implements GameMatcher {
+
+    @Override
+    public List<Game> matches(OrderedHand hand) {
+        Game toak = null;
+
+        Map<Card.CardNumber,List<Card>> cardsByRank = GameUtil.cardsByRank(hand);
+
+        if ( cardsByRank.size() == 3) {
+            for (List<Card> cardsInRank : cardsByRank.values()) {
+                if (cardsInRank.size() == 3) {
+                    toak = new Game(hand, Game.GameType.THREE_OF_A_KIND);
+                } else if (cardsInRank.size() != 1) {
+                    break;
+                }
+            }
+        }
+
+        return toak == null ? null : Arrays.asList(toak);
+    }
+}
